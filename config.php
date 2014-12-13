b1093bad401d73a121b60ce52ba852bdb293d004
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2014-12-13 08:00:39 +0330

    adding config+better table.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	return [
+		'db'=>[
+			'host'=>'localhost',
+			'username'=>'root', 
+			'password'=>'', 
+			'name'=>'bizsearch'
+		],
+		'payline'=>[
+			'key'=>'d402a-bac33-181d9-0c748-b331f58bfce6cbd87c07917b996c',
+			'gateway'=>'http://payline.ir/payment/gateway-'
+		]
+	];
+?>
