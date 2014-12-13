de101fa58a3147a01104b5cc7c48399f2f794186
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2014-12-13 08:02:11 +0330

    update config.php

--- config.php
+++ config.php
@@ -4,11 +4,7 @@
 			'host'=>'localhost',
 			'username'=>'root', 
 			'password'=>'', 
-			'name'=>'bizsearch'
-		],
-		'payline'=>[
-			'key'=>'d402a-bac33-181d9-0c748-b331f58bfce6cbd87c07917b996c',
-			'gateway'=>'http://payline.ir/payment/gateway-'
+			'name'=>'table_name'
 		]
 	];
 ?>
